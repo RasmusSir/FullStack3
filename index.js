688b97084176eec5fd300c5f9e13431b02831889
RasmusSir <121805931+RasmusSir@users.noreply.github.com>
2023-01-21 15:37:08 +0200

    Tehtävät 3.8. asti

--- index.js
+++ index.js
@@ -1,90 +1,117 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
+morgan.token('content', function(req,res){return (JSON.stringify(req.body))})
+
+app.use(morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms',
+      tokens.content(req,res),
+    ].join(' ')
+  }))
+
+//app.use(morgan('tiny'))
 app.use(express.json())
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2022-01-10T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-01-10T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-01-10T19:20:14.298Z",
-    important: true
-  }
-]
+let persons = [
+          { 
+            "name": "Arto Hellas", 
+            "number": "040-123456",
+            "id": 1
+          },
+          { 
+            "name": "Ada Lovelace", 
+            "number": "39-44-5323523",
+            "id": 2
+          },
+          { 
+            "name": "Dan Abramov", 
+            "number": "12-43-234345",
+            "id": 3
+          },
+          { 
+            "name": "Mary Poppendieck", 
+            "number": "39-23-6423122",
+            "id": 4
+          }
+        ]
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
+app.get('/info', (req, res) => {
+    res.send(`<p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date().toString()}</p>`)
   })
   
-  app.get('/api/notes', (req, res) => {
-    res.json(notes)
+  app.get('/api/persons', (req, res) => {
+    res.json(persons)
   })
 
-  app.get('/api/notes/:id', (request, response) => {
+  app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
+    const person = persons.find(person => person.id === id)
     
-    if (note) {
-      response.json(note)
-      console.log('nootti', note);
+    if (person) {
+      response.json(person)
+      console.log('nootti', person);
       
     } else {
-        console.log('nootti2', note);
+        console.log('nootti2', person);
         
       response.status(404).end()
     }
   })
 
 
-  app.delete('/api/notes/:id', (request, response) => {
+  app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
+    persons = persons.filter(person => person.id !== id)
   
     response.status(204).end()
   })
 
 
   const generateId = () => {
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => n.id))
-      : 0
-    return maxId + 1
+    const id = Math.floor(Math.random()*1000);
+    return id
   }
 
-  app.post('/api/notes', (request, response) => {
+  app.post('/api/persons', (request, response) => {
     const body = request.body
     console.log('body', body);
 
-    if (!body.content) {
+    if (!body.name) {
         return response.status(400).json({ 
-        error: 'content missing' 
+        error: 'name missing' 
+        })
+    }
+
+    if (!body.number) {
+        return response.status(400).json({ 
+        error: 'number missing' 
+        })
+    }
+    if (persons.some(person => person.name === body.name)) {
+        return response.status(400).json({ 
+        error: 'values must be unique' 
         })
     }
   
-    const note = {
-        content: body.content,
-        important: body.important || false,
+    const person = {
+        name: body.name,
+        number: body.number || false,
         id: generateId(),
       }
-    notes = notes.concat(note)
+    persons = persons.concat(person)
   
-    response.json(note)
+    response.json(person)
   })
 
 
-  const PORT = 3002
+  const PORT = 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
