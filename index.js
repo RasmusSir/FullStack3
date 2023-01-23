f7e596363f880c81c0cded538b633c579dfb84b2
RasmusSir <121805931+RasmusSir@users.noreply.github.com>
2023-01-23 16:10:02 +0200

    3.18. asti

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ const app = express()
 const Person = require('./models/person')
 
 
-app.use(cors())
+//MW: Express Static & Json
 app.use(express.static('build'))
+app.use(express.json())
+//MW: CORS
+app.use(cors())
 
+//MW: Morgan
 morgan.token('content', function (req, res) { return (JSON.stringify(req.body)) })
 app.use(morgan(function (tokens, req, res) {
   return [
@@ -22,18 +26,20 @@ app.use(morgan(function (tokens, req, res) {
   ].join(' ')
 }))
 
-app.use(express.json())
+
+
 
 
 //Infosivun luominen
-app.get('/info', (request, response) => {
+app.get('/info', (request, response, next) => {
   Person.countDocuments({}).then(count => {
-    response.send(`<p>Phonebook has info for ${count} people</p>
-    <p>${new Date().toString()}</p>`)
-  }).catch(error => {
-    console.log('The following app.get/info error should be handled somehow:', error);
+    response.send(
+      `<p>Phonebook has info for ${count} people</p>
+      <p>${new Date().toString()}</p>`)
   })
+    .catch(error => next(error))
 })
+
 
 // Henkilöiden hekaminen
 app.get('/api/persons', (request, response) => {
@@ -43,25 +49,30 @@ app.get('/api/persons', (request, response) => {
 })
 
 // Yksittäisen henkilön hakeminen
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  })
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 }
 )
 
 // Henkilön poistaminen listalta
-app.delete('/api/persons/:id', (request, response) => {
-  Person.findByIdAndDelete(request.params.id).then(person => {
-    if (!person === true) {
-      return response.status(404).json({ error: "Error: Cannot find person" })
-    }
-    response.status(204).end()
-  })
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 //Jo syötetyn henkilön numeron vaihtaminen
-app.put('/api/persons/:id', (request, response) => {
+app.put('/api/persons/:id', (request, response, next) => {
   const newBody = request.body
   const id = request.params.id
   console.log('Changing Name To', request.body);
@@ -70,15 +81,12 @@ app.put('/api/persons/:id', (request, response) => {
     .then(updatedPerson => {
       response.json(updatedPerson)
     })
-    .catch(error => {
-      console.log('The following app.put/persons/:id error should be handled somehow:', error);
-
-      next(error)
-    })
+    .catch(error => next(error)
+    )
 })
 
 // Uuden henkilön lisääminen
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
   console.log('Logging body in app.post', body);
 
@@ -94,21 +102,25 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
-  Person.findOne({ name: body.name }).then(personExisting => {
-    if (personExisting) {
-      return response.status(400).json({
-        error: 'values must be unique'
+  Person.findOne({ name: body.name })
+    .then(personExisting => {
+      if (personExisting) {
+        return response.status(400).json({
+          error: 'values must be unique'
+        })
+      }
+      const person = new Person({
+        name: body.name,
+        number: body.number || false,
       })
-    }
 
-    const person = new Person({
-      name: body.name,
-      number: body.number || false,
+      person.save()
+        .then(savedPerson => {
+          response.json(savedPerson)
+        })
+        .catch(error => next(error)
+        )
     })
-    person.save().then(savedPerson => {
-      response.json(savedPerson)
-    })
-  })
 })
 
 
@@ -116,3 +128,14 @@ const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+
+//MW: Error Handler
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  next(error)
+}
+app.use(errorHandler)
